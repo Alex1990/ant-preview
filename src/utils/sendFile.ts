0b720e1c68d3a3707798ff097543d7e7e117337c
Alex Chao <alexchao1990@gmail.com>
2021-05-26 00:16:19 +0800

    feat: handle the unsupported file

--- src/utils/sendFile.ts
+++ src/utils/sendFile.ts
@@ -1,20 +1,35 @@
-import { BrowserWindow } from 'electron'
+import { BrowserWindow, dialog } from 'electron'
 import fs from 'fs'
 import readChunk from 'read-chunk'
 import FileType from 'file-type'
 import isSvg from 'is-svg'
+import extensions from './extensions'
 
 const fsp = fs.promises
+
+function showNotSupportMessage() {
+  dialog.showErrorBox('Unsupported format', 'This extension of the file is not supported.')
+}
 
 export default async function sendFile(win: BrowserWindow, file: string): Promise<void> {
   const buffer = readChunk.sync(file, 0, 4100)
   const fileType = await FileType.fromBuffer(buffer)
   let mime = ''
   const data = await fsp.readFile(file)
+  let ext = ''
   if (fileType) {
     mime = fileType.mime
+    ext = fileType.ext
+    if (!extensions.includes(ext)) {
+      showNotSupportMessage()
+      return
+    }
   } else if (isSvg(data)) {
     mime = 'image/svg+xml'
+    ext = 'svg'
+  } else {
+    showNotSupportMessage()
+    return
   }
   win.webContents.send('open', {
     path: file,
