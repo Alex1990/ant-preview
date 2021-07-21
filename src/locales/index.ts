5c52ba729fad68d0984aa4f544f41dc4af62f17c
Alex Chao <alexchao1990@gmail.com>
2021-07-21 23:37:00 +0800

    feat: fallback to default locale (#3)

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,15 +1,33 @@
+import { isObject } from 'lodash'
 import enUS from './en-US.json'
 import zhCN from './zh-CN.json'
 
-export const locales = {
+export type Locale = 'en-US' | 'zh-CN'
+
+type LocaleData = typeof enUS
+
+type AllLocaleData = Record<Locale, unknown>
+
+export const locales: AllLocaleData = {
   'en-US': enUS,
   'zh-CN': zhCN
 }
 
-export type Locale = 'en-US' | 'zh-CN'
-
 export const defaultLocale = 'en-US'
 
-export function getLocaleData(locale: Locale): typeof locales['en-US'] {
-  return locales[locale] ? locales[locale] : locales[defaultLocale]
+function assign<T>(dest: T, src: T) {
+  for (const p of Object.keys(src) as Array<keyof T>) {
+    if (isObject(dest[p])) {
+      assign(dest[p], src[p])
+    } else if (dest[p] === undefined) {
+      dest[p] = src[p]
+    }
+  }
+}
+
+export function getLocaleData(locale: Locale): LocaleData {
+  const defaultLocaleData = locales[defaultLocale] as LocaleData
+  const localeData = (locales[locale] || {}) as LocaleData 
+  assign<LocaleData>(localeData, defaultLocaleData)
+  return localeData
 }
