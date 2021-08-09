ec11b15f24fed7ad5d13bbe8ec582e9ac60a2ab4
Alex Chao <alexchao1990@gmail.com>
2021-08-09 23:49:04 +0800

    feat: save settings into home directory (#14)

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,17 +1,25 @@
+import os from 'os'
 import fs from 'fs'
 import path from 'path'
+import pkg from '../../package.json'
 import { Settings } from '../types/Settings'
 
 const fsp = fs.promises
 
-const appRoot = process.cwd()
-const settingsFile = path.join(appRoot, 'settings.json')
+const settingsDir = path.join(os.homedir(), `.${pkg.name}`)
+const settingsFile = path.join(settingsDir, 'settings.json')
 
 const defaultSettings: Settings = {
   canvasBackgroundColor: '#f8f8f8'
 }
 
 let settings = ''
+
+try {
+  fs.accessSync(settingsDir)
+} catch (err) {
+  fs.mkdirSync(settingsDir)
+}
 
 try {
   const content = fs.readFileSync(settingsFile, 'utf8')
